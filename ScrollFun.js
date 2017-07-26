b8c527ca30de37b244e273ae665860a81eeabcef
Jonathan <jonrcao@gmail.com>
2017-07-26 11:33:36 +0800

    ud

--- ScrollFun.js
+++ ScrollFun.js
@@ -1,18 +1,50 @@
 var ScrollFun = function(option) {
-	// 默认配置
-	option.name ? option.name = option.name : option.name = "Jon";
+    // 默认配置
+    option.name ? option.name = option.name : option.name = "Jon";
 
-	// 初始化函数
-	ScrollFun.prototype.init = function() {
-		
-		$(option.elem).css('color', 'red');
-		
-		window.onscroll = function (){
-			var s = document.documentElement.scrollTop || document.body.scrollTop; 
-		    console.log(s);
-		}
-		
-		
-	}
+    // 初始化函数
+    ScrollFun.prototype.init = function() {
+        $(option.elem).each(function(i, j) {
+            $(j).css('visibility', 'hidden').addClass('animated');
+        });
+
+        // 首屏载入动画
+        var winHeigh = $(window).height();
+        $(option.elem).each(function(i, j) {
+            var elTop = $(j).offset().top;
+
+            if(winHeigh > elTop) {
+                var action = $(j).data('action');
+                action ? action = action : action = "zoomIn";
+                
+                setTimeout(function() {
+                    $(j).css('visibility', 'visible').addClass(action);
+                }, 0);
+            }
+
+        });
+        
+        // 监听滚动载入动画
+        window.onscroll = function() {
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var winHeigh = $(window).height();
+
+            $(option.elem).each(function(i, j) {
+                var elTop = $(j).offset().top;
+
+                if(scrollTop + winHeigh > elTop) {
+                    var action = $(j).data('action');
+                    action ? action = action : action = "zoomIn";
+                    
+                    setTimeout(function() {
+                        $(j).css('visibility', 'visible').addClass(action);
+                    }, 0);
+                }
+
+            });
+
+        }
+
+    }
 
 }
