9d5a53dd4ba0830271b704be8c8af0bf128960a6
Jonathan <jonrcao@gmail.com>
2017-07-26 15:36:25 +0800

    build web

--- dist/ScrollFun.js
+++ dist/ScrollFun.js
@@ -1,6 +1,6 @@
 var ScrollFun = function(option) {
     // 默认配置
-    option.name ? option.name = option.name : option.name = "Jon";
+    option.offset ? option.offset = option.offset : option.offset = "0"; // 开始播放动画距离
 
     // 初始化函数
     ScrollFun.prototype.init = function() {
@@ -9,40 +9,43 @@ var ScrollFun = function(option) {
         });
 
         // 首屏载入动画
-        var winHeigh = $(window).height();
-        $(option.elem).each(function(i, j) {
-            var elTop = $(j).offset().top;
+        commonAnimate();
 
-            if(winHeigh > elTop) {
-                var action = $(j).data('action');
-                action ? action = action : action = "zoomIn";
-                
-                setTimeout(function() {
-                    $(j).css('visibility', 'visible').addClass(action);
-                }, 0);
-            }
-
-        });
-        
         // 监听滚动载入动画
         window.onscroll = function() {
             var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            commonAnimate(scrollTop);
+        }
+
+        // 动画公用函数
+        function commonAnimate(scrollTop) {
             var winHeigh = $(window).height();
+            var offset = 0;
+
+            if (scrollTop) {
+                // 如果不是首屏
+                scrollTop = scrollTop;
+                offset = option.offset;
+            } else {
+                scrollTop = 0;
+                offset = 0;
+            }
 
             $(option.elem).each(function(i, j) {
                 var elTop = $(j).offset().top;
 
-                if(scrollTop + winHeigh > elTop) {
+                if(scrollTop + winHeigh - parseInt(offset) > elTop) {
                     var action = $(j).data('action');
+                    var delay = $(j).data('delay');
                     action ? action = action : action = "zoomIn";
-                    
+                    delay ? delay = delay : delay = "0";
+
                     setTimeout(function() {
                         $(j).css('visibility', 'visible').addClass(action);
-                    }, 0);
+                    }, parseInt(delay));
                 }
 
             });
-
         }
 
     }
